5282625f81bd8d289ace7d04e97553f7d69add13
Christian Stropp <christian.stropp@web.de>
2009-02-08 13:09:08 +0100

    Problem 5 solved

---
+++ problem005.rb
@@ -0,0 +1,40 @@
+# http://projecteuler.net/index.php?section=problems&id=5
+# solved 08.02.2009
+
+divisors = Array(1..20)
+result = 0
+found = false
+notneeded = Array.new
+
+# clean array
+for i in divisors
+  for j in divisors
+    if j / i > 1 && j % i == 0
+      notneeded.push(i)
+    end
+  end
+end
+
+# reverse order for correct exclusion and faster
+divisors.reverse!
+
+for k in -divisors.length..-1
+  if notneeded.include?(divisors[k])
+    divisors.delete_at(k)
+  end
+end
+
+puts divisors
+
+while !found do
+  found = true
+  result += divisors[0] #take steps of biggest divisor
+  divisors.each do |divisor|
+    if !(result % divisor == 0)
+      found = false
+      break #faster
+    end
+  end
+end
+
+puts result
